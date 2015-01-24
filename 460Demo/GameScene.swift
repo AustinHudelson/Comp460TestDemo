f364ccda29869a29f7c13a5a9c6217f056717e26
oyau <olyver.yau@gmail.com>
2015-01-24 15:13:41 -0600

    Dragging mechanism?

--- 460Demo/GameScene.swift
+++ 460Demo/GameScene.swift
@@ -10,8 +10,11 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var localSprite = SKSpriteNode(imageNamed:"Spaceship")
+    
     var PlayerSprites: [String:SKSpriteNode] = [:]
     let PlayerSpeed = CGFloat(100.0)
+    var objectTouched = false;
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -24,36 +27,64 @@ class GameScene: SKScene {
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
         self.addChild(myLabel)
+        localSprite.xScale = 0.5
+        localSprite.yScale = 0.5
+        localSprite.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        self.addChild(localSprite)
+        PlayerSprites[AppWarpHelper.sharedInstance.playerName] = localSprite
+//        self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+                        var loc2 = touch.locationInNode(self)
             
-            //Make a muteable dictionary to send over appwarp
-            var dataDict = NSMutableDictionary()
+                        var dist = pow(pow(localSprite.position.x - loc2.x,2)+pow(localSprite.position.y - loc2.y,2),0.5)
+                        if dist < 175
+                        {
+                            objectTouched = true
+                        }
             
-            //Store the userName/playerName in the sending dictionary
-            //Set the username for the muteable dictionary
-            dataDict.setObject(AppWarpHelper.sharedInstance.playerName, forKey: "userName")
+                        
+                        //sprite1.position = location
+                    }
+       
+    }
+    override func touchesEnded(touches:NSSet, withEvent event: UIEvent)
+    {
+        for touch: AnyObject in touches {
+            if(objectTouched == true)
+            {
+                let location = touch.locationInNode(self)
+                
+                //Make a muteable dictionary to send over appwarp
+                var dataDict = NSMutableDictionary()
+                
+                //Store the userName/playerName in the sending dictionary
+                //Set the username for the muteable dictionary
+                dataDict.setObject(AppWarpHelper.sharedInstance.playerName, forKey: "userName")
+                
+                
+                ////Convert the touched location to a string for projectile position
+                //var destStr:String = NSStringFromCGPoint(location)
+                //dataDict.setObject(destStr, forKey: "projectileDest")
+                
+                //Convert the player position to a string
+                var playerPosition:String = NSStringFromCGPoint(location)
+                //Add the string to the dictionary
+                dataDict.setObject(playerPosition, forKey: "playerPosition")
+                //dataDict.setObject(NSValue(CGPoint: player!.position), forKey: "playerPosition")
+                
+                //Notify Room of updated data
+                AppWarpHelper.sharedInstance.updatePlayerDataToServer(dataDict)
+                objectTouched = false
+            }
             
-            
-            ////Convert the touched location to a string for projectile position
-            //var destStr:String = NSStringFromCGPoint(location)
-            //dataDict.setObject(destStr, forKey: "projectileDest")
-            
-            //Convert the player position to a string
-            var playerPosition:String = NSStringFromCGPoint(location)
-            //Add the string to the dictionary
-            dataDict.setObject(playerPosition, forKey: "playerPosition")
-            //dataDict.setObject(NSValue(CGPoint: player!.position), forKey: "playerPosition")
-            
-            //Notify Room of updated data
-            AppWarpHelper.sharedInstance.updatePlayerDataToServer(dataDict)
         }
     }
+
     
     func updateEnemyStatus(dataDict: NSDictionary){
         println("Running Update Status")
@@ -63,15 +94,18 @@ class GameScene: SKScene {
         
         if let sprite = PlayerSprites[dataDict.objectForKey("userName") as String] {
             let charPos = sprite.position;
+            //localSprite.position = sprite.position
             let xdif = touchLoc.x-charPos.x
             let ydif = touchLoc.y-charPos.y
             
             let distance = sqrt((xdif*xdif)+(ydif*ydif))
             let duration = distance/PlayerSpeed
             let action = SKAction.moveTo(touchLoc, duration:NSTimeInterval(duration))
+            //sprite.runAction(action)
             sprite.runAction(action)
             
-        } else {
+        }
+        else {
             //This is a new user that has not been seen before
             //Create a new sprite for this user
             let newSprite = SKSpriteNode(imageNamed:"Spaceship")
