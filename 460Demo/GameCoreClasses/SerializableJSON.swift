e87ca793218b92ed73861133a1de39a9da60249f
rko@MacbookAir <rko@MacbookAir>
2015-02-20 14:29:40 -0600

    temporary commit for serialization

--- 460Demo/GameCoreClasses/SerializableJSON.swift
+++ 460Demo/GameCoreClasses/SerializableJSON.swift
@@ -20,27 +20,38 @@ func +=<K, V> (inout left: Dictionary<K, V>, right: Dictionary<K, V>) -> Diction
     return left
 }
 
-/*
-    Any class that is to be sent over the network should inheriet from this class so it can
-    be serialized into JSON
-*/
 class SerializableJSON: NSObject {
     
     /*
-        Convert object to a dictionary of properties.
-        Eg. if self = Warrior, we have a dictionary that looks something like:
-            {
-                "unitType": "Warrior"
-                "name": "warrior1"
-                "health": 100
-                "speed": 1.1
-            }
-        REF:
-        https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ObjCRuntimeRef/index.html
-        https://developer.apple.com/library/ios/documentation/Swift/Conceptual/BuildingCocoaApps/WritingSwiftClassesWithObjective-CBehavior.html
-        https://gist.github.com/turowicz/e7746a9c035356f9483d
-        http://blog.mailcloud.com/technology/apple-swift-code-strong-type-object-serialization-to-json/
-        http://stackoverflow.com/questions/25476740/how-can-i-test-if-an-instance-is-a-specific-class-or-type-in-swift
+    - A method that is shared between toJSON() and fromJSON()
+    - This method returns an array of properties in the given class
+    */
+    func propertyNames(aClass: AnyClass?) -> [String] {
+        var propNames: [String] = []
+        var propertiesCount: CUnsignedInt = 0
+        var propertiesInAClass: UnsafeMutablePointer<objc_property_t> = class_copyPropertyList(aClass, &propertiesCount)
+        
+        for var i = 0; i < Int(propertiesCount); i++ {
+            var property = propertiesInAClass[i]
+            var propName = String.fromCString(property_getName(property))!
+            propNames.append(propName)
+        }
+        free(propertiesInAClass)
+        return propNames
+    }
+    
+    /*
+    ===============================================================================
+    METHODS USED TO CONVERT THIS OBJECT TO A DICTIONARY ENTRY IN JSON
+    REF:
+    https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ObjCRuntimeRef/index.html
+    https://developer.apple.com/library/ios/documentation/Swift/Conceptual/BuildingCocoaApps/WritingSwiftClassesWithObjective-CBehavior.html
+    https://gist.github.com/turowicz/e7746a9c035356f9483d
+    http://blog.mailcloud.com/technology/apple-swift-code-strong-type-object-serialization-to-json/
+    http://stackoverflow.com/questions/25476740/how-can-i-test-if-an-instance-is-a-specific-class-or-type-in-swift
+    http://stackoverflow.com/questions/26630447/serializing-custom-bag-object-to-json-string-int-to-bool-in-swift
+    
+    - This method goes through this object's properties and serialize it into a JSON dictionary
     */
     func toJSON() -> [String: AnyObject] {
         var aClass: AnyClass? = self.dynamicType
@@ -52,7 +63,7 @@ class SerializableJSON: NSObject {
         propertiesDictionary += classPropertiesDict
         
         /*
-            - if this obj's superclass is a specific class (eg. Unit, Order...etc), then get that super class's properties too. We're only doing this check because sometimes we might get an Object whose superclass is not the class we're expecting (eg. NSObject), and we don't really want to serialize those superclass's properties
+        - if this obj's superclass is a specific class (eg. Unit, Order...etc), then get that super class's properties too. We're only doing this check because sometimes we might get an Object whose superclass is not the class we're expecting (eg. NSObject), and we don't really want to serialize those superclass's properties
         */
         if superClass === Unit.self || superClass === Order.self {
             var superClassPropertiesDict = toDictionary(superClass)
@@ -63,50 +74,55 @@ class SerializableJSON: NSObject {
     }
     
     /*
-        - This method should only be used by toJSON() above when you need to convert a class's (and its super class's)
-        properties into a dictionary
+    - This method should only be used by toJSON() above when you need to convert a class's (and its super class's)
+    properties into a dictionary
     */
     func toDictionary(aClass: AnyClass?) -> [String: AnyObject] {
-        var propertiesCount: CUnsignedInt = 0
-        var propertiesInAClass: UnsafeMutablePointer<objc_property_t> = class_copyPropertyList(aClass, &propertiesCount)
+        
+        
         var propertiesDictionary: [String: AnyObject] = [:]
         
-        for var i = 0; i < Int(propertiesCount); i++ {
-            var property = propertiesInAClass[i]
-            var propName = String.fromCString(property_getName(property))!
-            var propType = property_getAttributes(property)
+        for propName in propertyNames(aClass) {
+            //            var propType = property_getAttributes(property)
             var propValue : AnyObject! = self.valueForKey(propName);
             
             /*
-                - If this property's name is tagged with "DS_" (Don't Send), ignore it
+            - If this property's name contains the substring "DS_" (Don't Send), ignore it
+            REF: http://stackoverflow.com/questions/24034043/how-do-i-check-if-a-string-contains-another-string-in-swift
             */
+            if propName.rangeOfString("DS_") != nil {
+                continue
+            }
+            
             if propValue is SerializableJSON {
                 /*
-                    - This if statement recursively calls toJSONDict() when this particular property's value is itself a subclass
-                    of SerializableJSON
-                    - So for example, when you get a property that looks like this within a class:
-                    var order: Order
-                    you would basically recursively call order.toJSONDict()
+                - This if statement recursively calls toJSONDict() when this particular property's value is itself a subclass
+                of SerializableJSON
+                - So for example, when you get a property that looks like this within a class:
+                var order: Order
+                you would basically recursively call order.toJSONDict()
                 */
                 propertiesDictionary[propName] = (propValue as SerializableJSON).toJSON()
             } else if propValue is Array<SerializableJSON> {
                 /*
-                    - if this particular property's value is an array, serialize this array recursively
+                - if this particular property's value is an array, serialize this array recursively
                 */
                 var subArray = Array<AnyObject>()
                 for item in (propValue as Array<SerializableJSON>) {
                     subArray.append(item.toJSON())
                 }
                 propertiesDictionary[propName] = subArray
+            } else if propValue is SKSpriteNode {
+                /*
+                - Convert generally unserializable propValues, such as SKSpriteNode, into NSData and store it as an encoded string in the JSON file
+                */
+                let data: NSData = NSKeyedArchiver.archivedDataWithRootObject(propValue)
+                propertiesDictionary[propName] = data.base64EncodedStringWithOptions(nil)
             } else {
-                if !(propValue is SKSpriteNode) {
-                    propertiesDictionary[propName] = propValue
-                }
+                propertiesDictionary[propName] = propValue
             }
         }
         
-        propertiesInAClass.dealloc(Int(propertiesCount))
         return propertiesDictionary
     }
-    
 }
