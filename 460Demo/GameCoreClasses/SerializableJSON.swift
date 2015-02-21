f66055935c7d41d61c5150146cdd99446a78919a
AustinHudelson <austin.hudelson@yahoo.com>
2015-02-21 01:24:56 -0600

    pulled changed serialization and started implementation of init(dictionary) for unit

--- 460Demo/GameCoreClasses/SerializableJSON.swift
+++ 460Demo/GameCoreClasses/SerializableJSON.swift
@@ -13,16 +13,23 @@ import SpriteKit
     REF: http://stackoverflow.com/questions/24051904/how-do-you-add-a-dictionary-of-items-into-another-dictionary
     - Used to concatenate two dictionaries
 */
-public class SerializableJSON: NSObject {
-    
-    
+func +=<K, V> (inout left: Dictionary<K, V>, right: Dictionary<K, V>) -> Dictionary<K, V> {
+    for (k, v) in right {
+        left.updateValue(v, forKey: k)
+    }
+    return left
+}
+
+class SerializableJSON: NSObject {
     
     /*
     - A method that is shared between toJSON() and fromJSON()
     - This method returns an array of properties in the given class
     */
-    public func toJSONDict() -> [String: AnyObject] {
-        var output_dict: [String: AnyObject] = [:]
+    func propertyNames(aClass: AnyClass?) -> [String] {
+        var propNames: [String] = []
+        var propertiesCount: CUnsignedInt = 0
+        var propertiesInAClass: UnsafeMutablePointer<objc_property_t> = class_copyPropertyList(aClass, &propertiesCount)
         
         for var i = 0; i < Int(propertiesCount); i++ {
             var property = propertiesInAClass[i]
@@ -63,6 +70,61 @@ public class SerializableJSON: NSObject {
             // Put the superClass's properties in our results dictionary
             propertiesDictionary += superClassPropertiesDict
         }
+        return propertiesDictionary
+    }
+    
+    /*
+    - This method should only be used by toJSON() above when you need to convert a class's (and its super class's)
+    properties into a dictionary
+    */
+    func toDictionary(aClass: AnyClass?) -> [String: AnyObject] {
+        
+        
+        var propertiesDictionary: [String: AnyObject] = [:]
+        
+        for propName in propertyNames(aClass) {
+            //            var propType = property_getAttributes(property)
+            var propValue : AnyObject! = self.valueForKey(propName);
+            
+            /*
+            - If this property's name contains the substring "DS_" (Don't Send), ignore it
+            REF: http://stackoverflow.com/questions/24034043/how-do-i-check-if-a-string-contains-another-string-in-swift
+            */
+            if propName.rangeOfString("DS_") != nil {
+                continue
+            }
+            
+            if propValue is SerializableJSON {
+                /*
+                - This if statement recursively calls toJSONDict() when this particular property's value is itself a subclass
+                of SerializableJSON
+                - So for example, when you get a property that looks like this within a class:
+                var order: Order
+                you would basically recursively call order.toJSONDict()
+                */
+                propertiesDictionary[propName] = (propValue as SerializableJSON).toJSON()
+            } else if propValue is Array<SerializableJSON> {
+                /*
+                - if this particular property's value is an array, serialize this array recursively
+                */
+                var subArray = Array<AnyObject>()
+                for item in (propValue as Array<SerializableJSON>) {
+                    subArray.append(item.toJSON())
+                }
+                propertiesDictionary[propName] = subArray
+            } else if (propValue is SKSpriteNode) {
+                /*
+                - Convert generally unserializable propValues, such as SKSpriteNode, into NSData and store it as an encoded string in the JSON file
+                */
+//                let data: NSData = NSKeyedArchiver.archivedDataWithRootObject(propValue)
+//                propertiesDictionary[propName] = data.base64EncodedStringWithOptions(nil)
+            } else if (propValue is SKLabelNode || propValue is SKAction) {
+                // skip
+            } else {
+                propertiesDictionary[propName] = propValue
+            }
+        }
+        
         return propertiesDictionary
     }
     
