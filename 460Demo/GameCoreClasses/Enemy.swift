cae8a58065259cac38e553d7c44654bda6524c77
AustinHudelson <austin.hudelson@yahoo.com>
2015-03-12 21:06:11 -0500

    added new animations

--- 460Demo/GameCoreClasses/Enemy.swift
+++ 460Demo/GameCoreClasses/Enemy.swift
@@ -26,6 +26,8 @@ class Enemy: Unit, PType
         sprite = SKSpriteNode(imageNamed: "Mage")
         let spawnLoc = CGPoint(x: (receivedData["posX"] as CGFloat), y: (receivedData["posY"] as CGFloat))
         sprite.position = spawnLoc
+        let scaleAction: SKAction = SKAction.scaleTo(receivedData["scaleX"] as CGFloat, duration: NSTimeInterval(0.0))
+        self.sprite.runAction(scaleAction)
         self.sprite.runAction(self.DS_standAnim)
     }
     
@@ -38,8 +40,8 @@ class Enemy: Unit, PType
         self.maxhealth = 30
         self.healthregen = 1
         self.speed = 30.0
-        self.sprite.xScale = 3.0
-        self.sprite.yScale = 3.0
+        self.sprite.xScale = 2.0
+        self.sprite.yScale = 2.0
         self.isEnemy = true
         
         //Initializes all the DS_ animations
@@ -51,74 +53,128 @@ class Enemy: Unit, PType
     }
     
     /*
-    * Initializes the animations for this class
-    */
+     * Initializes the animations for this class
+     */
     func initializeAnimations(){
         //
         //Define Animations here
         //
         
-        let walkAtlas = SKTextureAtlas(named: "Walk")
-        let attackAtlas = SKTextureAtlas(named: "Attack")
-        let walkAnimName = "-Walk2"
-        let attackAnimName = "-Attack"
-        let standAnimName = "-Walk2"
+        let Atlas = SKTextureAtlas(named: "Mage")
+        let walkAnimName = "-walk"
+        let attackAnimName = "-attack"
+        let abilityAnimName = "-ability"
+        let deathAnimName = "-death"
+        let stumbleAnimName = "-stumble"
+        let standAnimName = "-walk"
         
         var walkTextures = SKAction.animateWithTextures([
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"0"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"1"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"2"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"3"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"4"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"5"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"6"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"7"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"8"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"9"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"10"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"11"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"12"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"13"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"14"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"15"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"16"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"17"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"18"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"19"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"20")
-            ], timePerFrame: 0.05, resize: true, restore: true)
+            Atlas.textureNamed(AnimationName+walkAnimName+"0"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"1"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"2"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"3"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"4"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"5"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"6"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"7"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"8"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"9"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"10"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"11"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"12"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"13"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"14"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"15"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"16"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"17"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"18"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"19"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"20"),
+            ], timePerFrame: 0.05)
         
         var attackTextures = SKAction.animateWithTextures([
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"0"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"1"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"2"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"3"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"4"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"5"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"6"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"7"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"8"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"9"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"10"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"11"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"12"),
-            ], timePerFrame: 0.05, resize: true, restore: true)
+            Atlas.textureNamed(AnimationName+attackAnimName+"0"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"1"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"2"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"3"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"4"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"5"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"6"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"7"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"8"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"9"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"10"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"11"),
+            ], timePerFrame: 0.05)
+        
+        var deathTextures = SKAction.animateWithTextures([
+            Atlas.textureNamed(AnimationName+deathAnimName+"0"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"1"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"2"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"3"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"4"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"5"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"6"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"7"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"8"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"9"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"10"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"11"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"12"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"13"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"14"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"15"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"16"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"17"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"18"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"19"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"20"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"21"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"22"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"23"),
+            ], timePerFrame: 0.05)
+        
+        var abilityTextures = SKAction.animateWithTextures([
+            Atlas.textureNamed(AnimationName+abilityAnimName+"0"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"1"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"2"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"3"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"4"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"5"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"6"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"7"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"8"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"9"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"10"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"11"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"12"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"13"),
+            ], timePerFrame: 0.1)
+        
+        var stumbleTextures = SKAction.animateWithTextures([
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"0"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"1"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"2"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"3"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"4")
+            ], timePerFrame: 0.05)
         
         var standTextures = SKAction.animateWithTextures([
-            walkAtlas.textureNamed(AnimationName+standAnimName+"0")
-            ], timePerFrame: 0.1, resize: true, restore: true)
+            Atlas.textureNamed(AnimationName+standAnimName+"0")
+            ], timePerFrame: 0.1)
         
         self.DS_walkAnim = SKAction.repeatActionForever(walkTextures)
         self.DS_standAnim = SKAction.repeatActionForever(standTextures)
         self.DS_attackAnim = SKAction.repeatAction(attackTextures, count: 1)
+        self.DS_stumbleAnim = SKAction.repeatAction(stumbleTextures, count: 1)
+        self.DS_deathAnim = SKAction.repeatAction(deathTextures, count: 1)
+        self.DS_abilityAnim = SKAction.repeatAction(abilityTextures, count: 1)
     }
     
     override func addUnitToGameScene(gameScene: GameScene, pos: CGPoint) {
         super.addUnitToGameScene(gameScene, pos: pos)
         self.sendOrder(RoamAttack(receiverIn: self))
-        self.applyTint(SKColor.redColor(), factor: 0.75, blendDuration: NSTimeInterval(0.0))
+        //self.applyTint(SKColor.redColor(), factor: 0.75, blendDuration: NSTimeInterval(0.0))
 
     }
-    
-    
 }
--- 460Demo/GameCoreClasses/Warrior.swift
+++ 460Demo/GameCoreClasses/Warrior.swift
@@ -23,10 +23,14 @@ class Warrior: Unit, PType
         
         //Initializes all the DS_ animations
         initializeAnimations()
-        /* Sprite setup */
+        /* Sprite setup. Needs to be done in every subclass of Unit */
         sprite = SKSpriteNode(imageNamed: "Mage")
         let spawnLoc = CGPoint(x: (receivedData["posX"] as CGFloat), y: (receivedData["posY"] as CGFloat))
         sprite.position = spawnLoc
+        self.sprite.xScale = receivedData["scaleX"] as CGFloat
+        self.sprite.yScale = receivedData["scaleY"] as CGFloat
+        let scaleAction: SKAction = SKAction.scaleTo(receivedData["scaleX"] as CGFloat, duration: NSTimeInterval(0.0))
+        self.sprite.runAction(scaleAction)
         self.sprite.runAction(self.DS_standAnim)
     }
     
@@ -39,8 +43,8 @@ class Warrior: Unit, PType
         self.maxhealth = 100
         self.healthregen = 1
         self.speed = 50.0
-        self.sprite.xScale = 1.00
-        self.sprite.yScale = 1.00
+        self.sprite.xScale = 3.00
+        self.sprite.yScale = 3.00
         self.isEnemy = false
         
         
@@ -61,58 +65,114 @@ class Warrior: Unit, PType
         //Define Animations here
         //
         
-        let walkAtlas = SKTextureAtlas(named: "Walk")
-        let attackAtlas = SKTextureAtlas(named: "Attack")
-        let walkAnimName = "-walk2"
+        let Atlas = SKTextureAtlas(named: "Warrior")
+        let walkAnimName = "-walk"
         let attackAnimName = "-attack"
-        let standAnimName = "-walk2"
+        let abilityAnimName = "-ability"
+        let deathAnimName = "-death"
+        let stumbleAnimName = "-stumble"
+        let standAnimName = "-walk"
         
         var walkTextures = SKAction.animateWithTextures([
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"1"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"2"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"3"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"4"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"5"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"6"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"7"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"8"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"9"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"10"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"11"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"12"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"13"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"14"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"15"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"16"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"17"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"18"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"19"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"20"),
-            walkAtlas.textureNamed(AnimationName+walkAnimName+"21")
+            Atlas.textureNamed(AnimationName+walkAnimName+"0"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"1"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"2"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"3"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"4"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"5"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"6"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"7"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"8"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"9"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"10"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"11"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"12"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"13"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"14"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"15"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"16"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"17"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"18"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"19"),
+            Atlas.textureNamed(AnimationName+walkAnimName+"20"),
             ], timePerFrame: 0.05)
         
         var attackTextures = SKAction.animateWithTextures([
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"0"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"1"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"2"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"3"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"4"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"5"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"6"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"7"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"8"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"9"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"10"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"11"),
-            attackAtlas.textureNamed(AnimationName+attackAnimName+"12"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"0"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"1"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"2"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"3"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"4"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"5"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"6"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"7"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"8"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"9"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"10"),
+            Atlas.textureNamed(AnimationName+attackAnimName+"11"),
+            ], timePerFrame: 0.05)
+        
+        var deathTextures = SKAction.animateWithTextures([
+            Atlas.textureNamed(AnimationName+deathAnimName+"0"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"1"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"2"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"3"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"4"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"5"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"6"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"7"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"8"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"9"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"10"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"11"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"12"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"13"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"14"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"15"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"16"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"17"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"18"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"19"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"20"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"21"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"22"),
+            Atlas.textureNamed(AnimationName+deathAnimName+"23"),
+            ], timePerFrame: 0.05)
+        
+        var abilityTextures = SKAction.animateWithTextures([
+            Atlas.textureNamed(AnimationName+abilityAnimName+"0"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"1"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"2"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"3"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"4"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"5"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"6"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"7"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"8"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"9"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"10"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"11"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"12"),
+            Atlas.textureNamed(AnimationName+abilityAnimName+"13"),
+            ], timePerFrame: 0.1)
+        
+        var stumbleTextures = SKAction.animateWithTextures([
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"0"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"1"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"2"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"3"),
+            Atlas.textureNamed(AnimationName+stumbleAnimName+"4")
             ], timePerFrame: 0.05)
         
         var standTextures = SKAction.animateWithTextures([
-            walkAtlas.textureNamed(AnimationName+standAnimName+"0")
+            Atlas.textureNamed(AnimationName+standAnimName+"0")
             ], timePerFrame: 0.1)
         
         self.DS_walkAnim = SKAction.repeatActionForever(walkTextures)
         self.DS_standAnim = SKAction.repeatActionForever(standTextures)
         self.DS_attackAnim = SKAction.repeatAction(attackTextures, count: 1)
+        self.DS_stumbleAnim = SKAction.repeatAction(stumbleTextures, count: 1)
+        self.DS_deathAnim = SKAction.repeatAction(deathTextures, count: 1)
+        self.DS_abilityAnim = SKAction.repeatAction(abilityTextures, count: 1)
     }
 }
