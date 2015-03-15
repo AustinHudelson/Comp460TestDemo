8fa1497cad4f808a83fb49d7df6122553d4213d5
rko@MacbookAir <robertko1312@gmail.com>
2015-03-15 03:06:58 -0500

    committing to resolve merge conflict

--- 460Demo/GameCoreClasses/Mage.swift
+++ 460Demo/GameCoreClasses/Mage.swift
@@ -48,6 +48,7 @@ class Mage: Unit, PType
         
         /* Initialize Warrior buttons. They will automatically be added to the scene */
         let Button0: Ability = ButtonAreaHeal(slot: 0)
+        let Button1: Ability = ButtonShrink(slot: 1)
     }
     
     /*
