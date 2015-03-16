b6374bb5e763eb0dbcd416b783e8e0f3029ba4e1
oyau <olyver.yau@gmail.com>
2015-03-15 23:32:48 -0500

    fixed deleting room

--- 460Demo/AppWarpHelperClasses/NotificationListener.swift
+++ 460Demo/AppWarpHelperClasses/NotificationListener.swift
@@ -26,7 +26,11 @@ class NotificationListener: NSObject,NotifyListener
         if username == oldHost {
             WarpClient.getInstance().getLiveRoomInfo(AppWarpHelper.sharedInstance.roomId)
         }
-        WarpClient.getInstance().unsubscribeRoom(AppWarpHelper.sharedInstance.roomId)
+        if username == AppWarpHelper.sharedInstance.playerName
+        {
+            WarpClient.getInstance().unsubscribeRoom(AppWarpHelper.sharedInstance.roomId)
+    
+        }
     }
     
     func onUserResumed(userName: String!, withLocation locId: String!, isLobby: Bool)
